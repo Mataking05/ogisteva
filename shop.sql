c08143becbf8a3060b76c571e3282735b7e2ac90
MarkovicMihajlo <mihajlomarkovic003@gmail.com>
2024-06-14 16:09:28 +0200

    nova

--- shop.sql
+++ shop.sql
@@ -14,10 +14,37 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 -- Dumping database structure for shop
 CREATE DATABASE IF NOT EXISTS `shop` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `shop`;
+
+-- Dumping structure for table shop.kategorija
+CREATE TABLE IF NOT EXISTS `kategorija` (
+  `kategorija_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ime` varchar(255) NOT NULL,
+  PRIMARY KEY (`kategorija_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping structure for table shop.uloga
+CREATE TABLE IF NOT EXISTS `uloga` (
+  `uloga_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ime` varchar(255) NOT NULL,
+  PRIMARY KEY (`uloga_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Dumping structure for table shop.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uloga_id` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `is_active` int(10) unsigned NOT NULL DEFAULT 0,
+  PRIMARY KEY (`user_id`),
+  KEY `fk_user_uloga_id` (`uloga_id`),
+  CONSTRAINT `fk_user_uloga_id` FOREIGN KEY (`uloga_id`) REFERENCES `uloga` (`uloga_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Dumping structure for table shop.gitara
 CREATE TABLE IF NOT EXISTS `gitara` (
@@ -29,21 +56,6 @@ CREATE TABLE IF NOT EXISTS `gitara` (
   KEY `fk_gitara_kategorija_id` (`kategorija_id`),
   CONSTRAINT `fk_gitara_kategorija_id` FOREIGN KEY (`kategorija_id`) REFERENCES `kategorija` (`kategorija_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Dumping data for table shop.gitara: ~0 rows (approximately)
-
--- Dumping structure for table shop.kategorija
-CREATE TABLE IF NOT EXISTS `kategorija` (
-  `kategorija_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ime` varchar(255) NOT NULL,
-  PRIMARY KEY (`kategorija_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Dumping data for table shop.kategorija: ~3 rows (approximately)
-INSERT INTO `kategorija` (`kategorija_id`, `ime`) VALUES
-	(1, 'Elektricna'),
-	(2, 'Akusticna '),
-	(3, 'Klasicna');
 
 -- Dumping structure for table shop.korpa
 CREATE TABLE IF NOT EXISTS `korpa` (
@@ -58,34 +70,16 @@ CREATE TABLE IF NOT EXISTS `korpa` (
   CONSTRAINT `fk_korpa_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Dumping data for table shop.korpa: ~0 rows (approximately)
-
--- Dumping structure for table shop.uloga
-CREATE TABLE IF NOT EXISTS `uloga` (
-  `uloga_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `ime` varchar(255) NOT NULL,
-  PRIMARY KEY (`uloga_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+-- Dumping data for table shop.kategorija: ~3 rows (approximately)
+INSERT INTO `kategorija` (`kategorija_id`, `ime`) VALUES
+  (1, 'Elektricna'),
+  (2, 'Akusticna '),
+  (3, 'Klasicna');
 
 -- Dumping data for table shop.uloga: ~2 rows (approximately)
 INSERT INTO `uloga` (`uloga_id`, `ime`) VALUES
-	(1, 'Administrator'),
-	(2, 'Korisnik');
-
--- Dumping structure for table shop.user
-CREATE TABLE IF NOT EXISTS `user` (
-  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `uloga_id` int(10) unsigned NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `is_active` int(10) unsigned NOT NULL DEFAULT 0,
-  PRIMARY KEY (`user_id`),
-  KEY `fk_user_uloga_id` (`uloga_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- Dumping data for table shop.user: ~0 rows (approximately)
+  (1, 'Administrator'),
+  (2, 'Korisnik');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
